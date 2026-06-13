457b30b29d87a9238a3621bb63803d347c7d854b
suryaprakashk008 <suryaprakashk24082003@gmail.com>
2026-06-13 14:24:37 +0530

    added search bar for searching student name and also show loading animation in input search box

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,19 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const roboto = Roboto({
   subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
 });
 
 export const metadata = {
@@ -20,9 +25,9 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className= "h-full antialiased"
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`${roboto.className} min-h-full flex flex-col`}>{children}</body>
     </html>
   );
 }
